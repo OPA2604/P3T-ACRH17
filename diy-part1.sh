f65aaa7189c295875aea7bca2cd92952e2f59c6f
OPA2604 <70869445+OPA2604@users.noreply.github.com>
2021-02-15 16:45:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,9 +11,14 @@
 #
 
 # Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#src-git helloworld https://github.com/fw876/helloworld
+#src-git passwall https://github.com/xiaorouji/openwrt-passwall
+#src-git small https://github.com/kenzok8/small
+#添加Passwall插件
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
