9557ced7f940db48fb241f8adf0bd869fe51a485
OPA2604 <70869445+OPA2604@users.noreply.github.com>
2021-02-15 16:46:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.22.1/g' package/base-files/files/bin/config_generate
+
+# Modify hostname
+sed -i 's/OpenWrt/R2S/g' package/base-files/files/bin/config_generate
