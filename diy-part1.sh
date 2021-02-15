843d2cbb0af721f26d3463bf6da775bb7c87925c
OPA2604 <70869445+OPA2604@users.noreply.github.com>
2021-02-15 22:09:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 # Add a feed source
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+
