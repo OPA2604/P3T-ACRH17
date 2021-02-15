4a07f4ac90165813f271867fe969035d9ccc8806
OPA2604 <70869445+OPA2604@users.noreply.github.com>
2021-02-15 20:11:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,3 +1,4 @@
+  
 #!/bin/bash
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
@@ -11,8 +12,4 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.22.1/g' package/base-files/files/bin/config_generate
-
-# Modify hostname
-sed -i 's/OpenWrt/R2S/g' package/base-files/files/bin/config_generate
